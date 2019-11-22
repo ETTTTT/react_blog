a86738e6247e0c3c7c2dd6d7bbf1186a2ed74d97
wl <13763042163@163.com>
2019-11-22 17:51:35 +0800

    更新21集内容

--- blog/pages/detailed.js
+++ blog/pages/detailed.js
@@ -15,11 +15,28 @@ import marked from 'marked'
 import hljs from 'highlight.js'
 import 'highlight.js/styles/monokai-sublime.css'
 
-
+import Tocify from '../components/tocify.tsx'
 
 const Detailed = (props) => {
+  const tocify = new Tocify()
+  console.log(tocify)
   const renderer = new marked.Renderer()
-
+  // renderer.heading = (text, level, raw) => {
+  //   const anchor = tocify.add(text, level)
+  //   return `<a id="${anchor}" href="#${anchor}" class="anchor-fix"><h${level}>${text}</h${level}></a>\n`;
+  // }
+  renderer.heading = function(text, level, raw) {
+    const anchor = tocify.add(text, level);
+    return `<a id="${anchor}" href="#${anchor}" class="anchor-fix"><h${level}>${text}</h${level}></a>\n`;
+  };
+  // renderer: 这个是必须填写的，你可以通过自定义的Renderer渲染出自定义的格式
+  // gfm：启动类似Github样式的Markdown,填写true或者false
+  // pedatic：只解析符合Markdown定义的，不修正Markdown的错误。填写true或者false
+  // sanitize: 原始输出，忽略HTML标签，这个作为一个开发人员，一定要写flase
+  // tables： 支持Github形式的表格，必须打开gfm选项
+  // breaks: 支持Github换行符，必须打开gfm选项，填写true或者false
+  // smartLists：优化列表输出，这个填写ture之后，你的样式会好看很多，所以建议设置成ture
+  // highlight: 高亮显示规则 ，这里我们将使用highlight.js来完成
   marked.setOptions({
     renderer: renderer,
     gfm: true, // 启动类似github的marked
@@ -76,11 +93,9 @@ let html = marked(props.article_content)
             <Affix offsetTop={5}>
               <div className="detailed-nav comm-box">
                   <div className="nav-title">文章目录</div>
-                  <MarkNav
-                    className="article-menu"
-                    source={html}
-                    ordered={false}
-                  />
+                  <div className="toc-list">
+                    {tocify && tocify.render()}
+                  </div>
               </div>
             </Affix>
           </Col>
